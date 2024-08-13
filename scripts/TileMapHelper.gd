afbc5ba71182745b2aefc30447c6c4443aaef614
Sciman101 <Sciman101@proton.me>
2024-08-12 21:01:55 -0400

    Finished blockade room

--- scripts/TileMapHelper.gd
+++ scripts/TileMapHelper.gd
@@ -1,18 +1,22 @@
 extends TileMap
 
 var overlay_layer_index : int
+var background_layer_index : int
 var overlay_layer_hidden : bool = true
 
 func _ready():
 	for i in get_layers_count():
-		if get_layer_name(i) == "Overlay":
+		var n = get_layer_name(i)
+		if n == "Overlay":
 			overlay_layer_index = i
-			break
-	# Just in case we disabled it for debugging
+		if n == "Background":
+			background_layer_index = i
+	# Just in case we disabled it for debuggin
 	set_layer_enabled(overlay_layer_index, true)
+	set_layer_enabled(background_layer_index, true)
 
 func _use_tile_data_runtime_update(layer: int, coords: Vector2i) -> bool:
-	return layer == overlay_layer_index
+	return layer == overlay_layer_index or layer == background_layer_index
 
 func _tile_data_runtime_update(layer: int, coords: Vector2i, tile_data: TileData) -> void:
 	# For the overlay layer, turn off any collisions
