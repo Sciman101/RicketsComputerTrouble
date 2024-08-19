4e452c22d8a6256d14d7525719efe3ea6e1eb9ff
Sciman101 <Sciman101@proton.me>
2024-08-19 18:24:19 -0400

    Added buddy system. heh.

---
+++ scripts/objects/buddy.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@onready var sprite = $AnimatedSprite2D
+
+@export var spriteframes : SpriteFrames
+@export var offset : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if name == 'Buddy':
+		push_error("Forgot to rename buddy!")
+	sprite.offset = offset
+	sprite.sprite_frames = spriteframes
+	if Stats.buddy_has_been_visited(name):
+		sprite.play("visited")
+	else:
+		sprite.play('default')
+
+func _on_body_entered(body):
+	if not Stats.buddy_has_been_visited(name):
+		sprite.play("visited")
+		Ui.show_popup("You found a buddy!")
--- scripts/singleton/Stats.gd
+++ scripts/singleton/Stats.gd
@@ -1,5 +1,16 @@
 extends Node
 
+var visited_buddies = {}
+
 var deaths := 0
 var laptops := 0
 var laptops_total := 0
+var buddies := 0
+
+func buddy_has_been_visited(buddy_name):
+	return visited_buddies.get(buddy_name, false)
+
+func visit_buddy(buddy_name):
+	if not buddy_has_been_visited(buddy_name):
+		buddies += 1
+		visited_buddies[buddy_name] = true
