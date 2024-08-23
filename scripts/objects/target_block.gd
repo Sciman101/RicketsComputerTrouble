3a326a01f75c5900e62d52812135345cb4c4a285
Sciman101 <Sciman101@proton.me>
2024-08-23 09:36:15 -0400

    Added target flash and linked rooms

--- scripts/objects/target_block.gd
+++ scripts/objects/target_block.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 const FeatherBurst = preload("res://partial/effects/feather_burst.tscn")
 
 func on_shot(_motion):
-	var burst = Utils.spawn(FeatherBurst, global_position, get_parent())
+	var burst = Utils.spawn(FeatherBurst, global_position + Vector2(16,16), get_parent())
 	burst.direction = _motion.normalized()
 	burst.one_shot = true
 	burst.finished.connect(burst.queue_free)
