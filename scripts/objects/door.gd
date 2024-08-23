1497191b91431765c6328065dfc37b0f17f42475
Sciman101 <Sciman101@proton.me>
2024-08-23 10:28:45 -0400

    Door sprite

--- scripts/objects/door.gd
+++ scripts/objects/door.gd
@@ -4,23 +4,32 @@ extends Area2D
 @export_file("*.tscn", "*.scn") var scene_to_load
 @export var default_spawn : bool = false
 
+@onready var sprite = $Sprite
+
 var overlapping : bool
+var has_connected_door : bool = true
 
 func _ready():
+	sprite.frame = 0
 	set_process_unhandled_input(true)
 	if linked_door_name.length() == 0:
 		modulate = Color.GRAY
+		has_connected_door = false
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		overlapping = true
+		if has_connected_door:
+			sprite.play("open")
 
 func _on_body_exited(body):
 	if body.is_in_group("Player"):
 		overlapping = false
+		if has_connected_door:
+			sprite.play("close")
 
 func _unhandled_input(event):
 	if overlapping:
 		if Input.is_action_just_pressed("up") and not RoomManager.is_transitioning:
-			if scene_to_load:
+			if has_connected_door:
 				RoomManager.goto(scene_to_load, linked_door_name)
